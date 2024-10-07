23784e1014448eea3243e4c62a330067e9cdf706
unknown <m.rakovskaia@gmail.com>
2024-10-07 12:14:38 +0200

    personalised

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let myProjects = document.getElementById("myProjects");
+myProjects.addEventListener('click', function nextPart() {
+    document.getElementById("portfolio").scrollIntoView(true);
+    });
+
+document.getElementById("myCompetences").addEventListener('click', function nextPart() {
+    document.getElementById("competences").scrollIntoView(true);
+    });
+
+document.getElementById("myContacts").addEventListener('click', function nextPart() {
+    document.getElementById("contact").scrollIntoView(true);
+    })
